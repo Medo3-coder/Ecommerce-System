ddf4ad78d6595e1e9c08e15c5c01dd5ce8dece43
MOHAMED FAROUK <59924879+Medo3-coder@users.noreply.github.com>
2022-03-19 10:41:32 +0200

    User Profile Design Part 1 no Img or user Img

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,64 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-           Hi...  {{ Auth::user()->name }}
-        </h2>
-    </x-slot>
+@extends('frontend.main_master')
+@section('title', 'Dashborad')
 
-    <div class="py-12">
-        this is home page
+@section('content')
+
+    <div class="body-content">
+
+        <div class="container">
+
+            <div class="row">
+                <div class="col-md-2">
+                    <br>
+                    @if (!empty($adminData->profile_photo_path))
+                        <img class="card-img-top" style="border-radius: 50% ;width: 170px ; height:200px"
+                            src="{{ asset('upload/admin_images/' . $adminData->profile_photo_path) }}"
+                            style="width: 100px ; height:100px">
+                    @else
+                        <img class="card-img-top" style="border-radius: 50% ;width: 170px ; height:200px"
+                            src="{{ asset('upload/no_image.jpg') }}">
+                    @endif
+
+                    <br><br>
+                    <ul class="list-group list-group-flush">
+
+                        <a href="" class="btn btn-primary btn-sm btn-block"> Home </a>
+                        <a href="" class="btn btn-primary btn-sm btn-block"> Profile Update </a>
+                        <a href="" class="btn btn-primary btn-sm btn-block"> Change Password </a>
+                        <a href="" class="btn btn-danger btn-sm btn-block"> Logout </a>
+
+
+                    </ul>
+
+
+                </div> {{-- end col md 2 --}}
+
+                <div class="col-md-2">
+
+
+                </div> {{-- end col md 2 --}}
+
+
+                <div class="col-md-6">
+
+                    <div class="card">
+                        <h3 class="text-center">
+                            <span class="text-danger"> Hi...</span> <strong>{{ Auth::user()->name }}</strong>
+                            Welcome To Our Shop
+                            <h3>
+
+                    </div>
+
+
+
+                </div> {{-- end col md 6 --}}
+
+
+            </div> {{-- end row --}}
+
+        </div>
+
     </div>
-</x-app-layout>
+
+
+@endsection
