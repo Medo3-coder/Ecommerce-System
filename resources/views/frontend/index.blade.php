7724ff5570c24bb2deeb1e9849add49bbff53751
MOHAMED FAROUK <59924879+Medo3-coder@users.noreply.github.com>
2022-05-29 14:11:51 +0200

    Product Show With Skip Category & Brand Part 1

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -698,7 +698,7 @@
                                                         <!-- /.product-image -->
 
                                                         <div class="product-info text-left">
-                                                            <h3 class="name"><a href="detail.html">
+                                                            <h3 class="name"><a href="{{ url('/product/details/' . $product->id . '/' . $product->product_slug_en) }}">
                                                                     @if (session()->get('language') == 'hindi')
                                                                         {{ $product->product_name_hin }}
                                                                     @elseif(session()->get('language') == 'arabic')
@@ -848,7 +848,7 @@
                                         <!-- /.product-image -->
 
                                         <div class="product-info text-left">
-                                            <h3 class="name"><a href="detail.html">
+                                            <h3 class="name"><a href="{{ url('/product/details/' . $product->id . '/' . $product->product_slug_en) }}">
                                                     @if (session()->get('language') == 'hindi')
                                                         {{ $product->product_name_hin }}
                                                     @elseif (session()->get('language') == 'arabic')
@@ -908,7 +908,142 @@
                     <!-- /.home-owl-carousel -->
                 </section>
                 <!-- /.section -->
-                <!-- ============================================== FEATURED PRODUCTS : END ============================================== -->
+                <!-- ================== FEATURED PRODUCTS : END ============================ -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <!-- ============================================== skip_product_0 PRODUCTS ============================================== -->
+  <section class="section featured-product wow fadeInUp">
+    <h3 class="section-title">
+
+
+        @if (session()->get('language') == 'hindi')
+        {{ $skip_category_0->category_name_hin }}
+    @elseif (session()->get('language') == 'arabic')
+        {{ $skip_category_0->category_name_ar }}
+    @else
+        {{ $skip_category_0->category_name_en }}
+    @endif
+    </h3>
+    <div class="owl-carousel home-owl-carousel custom-carousel owl-theme outer-top-xs">
+
+        @foreach ($skip_product_0 as $product)
+            <div class="item item-carousel">
+                <div class="products">
+                    <div class="product">
+                        <div class="product-image">
+                            <div class="image">
+                                <a
+                                    href="{{ url('/product/details/' . $product->id . '/' . $product->product_slug_en) }}"><img
+                                        src="{{ asset($product->product_thambnail) }}" alt=""></a>
+                            </div>
+                            <!-- /.image -->
+
+                            @php
+                                $discountAmount = $product->selling_price - $product->discount_price;
+                                $netPrice = ($discountAmount / $product->selling_price) * 100;
+                                $discount_percent = 100 - $netPrice;
+                                $discount_percentage = round($discount_percent);
+                            @endphp
+
+                            @if ($product->discount_price == null)
+                                <div class="tag hot"><span>hot</span></div>
+                            @else
+                                <div class="tag hot"><span>{{ $discount_percentage }}%</span>
+                                </div>
+                            @endif
+
+                        </div>
+                        <!-- /.product-image -->
+
+                        <div class="product-info text-left">
+                            <h3 class="name"><a href="detail.html">
+                                    @if (session()->get('language') == 'hindi')
+                                        {{ $product->product_name_hin }}
+                                    @elseif (session()->get('language') == 'arabic')
+                                        {{ $product->product_name_ar }}
+                                    @else
+                                        {{ $product->product_name_en }}
+                                    @endif
+                                </a>
+                            </h3>
+                            <div class="rating rateit-small"></div>
+                            <div class="description"></div>
+
+                            @if ($product->discount_price == null)
+                                <div class="product-price"> <span class="price">
+                                        ${{ $product->selling_price }} </span>
+                            @endif
+                            <div class="product-price"> <span class="price">
+                                    ${{ $discountAmount }} </span>
+                                <span class="price-before-discount">${{ $product->selling_price }}
+                                </span>
+                            </div>
+                            <!-- /.product-price -->
+
+                        </div>
+                        <!-- /.product-info -->
+                        <div class="cart clearfix animate-effect">
+                            <div class="action">
+                                <ul class="list-unstyled">
+                                    <li class="add-cart-button btn-group">
+                                        <button class="btn btn-primary icon" data-toggle="dropdown"
+                                            type="button"> <i class="fa fa-shopping-cart"></i> </button>
+                                        <button class="btn btn-primary cart-btn" type="button">Add to
+                                            cart</button>
+                                    </li>
+                                    <li class="lnk wishlist"> <a class="add-to-cart"
+                                            href="detail.html" title="Wishlist"> <i
+                                                class="icon fa fa-heart"></i> </a> </li>
+                                    <li class="lnk"> <a class="add-to-cart"
+                                            href="detail.html" title="Compare"> <i class="fa fa-signal"
+                                                aria-hidden="true"></i> </a> </li>
+                                </ul>
+                            </div>
+                            <!-- /.action -->
+                        </div>
+                        <!-- /.cart -->
+                    </div>
+                    <!-- /.product -->
+
+                </div>
+                <!-- /.products -->
+            </div>
+            <!-- /.item -->
+        @endforeach
+
+
+    </div>
+    <!-- /.home-owl-carousel -->
+</section>
+<!-- /.section -->
+<!-- ================== skip_product_0 PRODUCTS : END ============================ -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                 <!-- ============================================== WIDE PRODUCTS ============================================== -->
                 <div class="wide-banners wow fadeInUp outer-bottom-xs">
                     <div class="row">
