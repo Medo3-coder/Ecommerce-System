977ee0d761c329cdf4d53cec69cacaa736db5ebe
MOHAMED FAROUK <59924879+Medo3-coder@users.noreply.github.com>
2022-06-11 01:45:56 +0200

    check if item is exists in wishlist part3

--- app/Http/Controllers/User/WishlistController.php
+++ app/Http/Controllers/User/WishlistController.php
@@ -14,16 +14,27 @@ class WishlistController extends Controller
     public function addToWishlist(Request $request, $product_id)
     {
 
-        if (Auth::check()) {
+        if (Auth::check())
+        {
+              $exists = Wishlist::where('user_id', Auth::id())->where('product_id', $product_id)->first();
 
-            $exists = Wishlist::where('user_id', Auth::id())->where('product_id', $product_id)->first();
+              if(!$exists)
+              {
+                Wishlist::create([
+                    'user_id' => Auth::id(),
+                    'product_id' => $product_id,
+                ]);
+                return response()->json(['success' => 'Product added to wishlist']);
+              }
+              else
+              {
+                return response()->json(['error' => 'Product already in wishlist']);
+              }
 
-            Wishlist::create([
-                'user_id' => Auth::id(),
-                'product_id' => $product_id,
-            ]);
-            return response()->json(['success' => 'Product added to wishlist']);
-        } else {
+
+        }
+         else
+     {
             return response()->json(['error' => 'At First Login Your Account']);
         }
     }
--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -664,12 +664,11 @@
                                             <div class="action">
                                                 <ul class="list-unstyled">
                                                     <li class="add-cart-button btn-group">
-                                                        <button class="btn btn-primary icon" data-toggle="dropdown"
-                                                            type="button"> <i class="fa fa-shopping-cart" data-toggle="modal" data-target="#exampleModal" id="{{ $product->id }}" onclick="productView(this.id)">
-                                                                </i> </button>
-                                                        <button class="btn btn-primary cart-btn" type="button">Add to
-                                                            cart</button>
-                                                    </li>
+                                                        <button class="btn btn-primary icon" data-toggle="modal" type="button"  data-target="#exampleModal" id="{{ $product->id }}" onclick="productView(this.id)"> <i class="fa fa-shopping-cart"></i> </button>
+
+                                                        <button class="btn btn-primary cart-btn" type="button">Add to cart</button>
+
+                                                       </li>
 
 
                                                          <button class="btn btn-primary icon" type="button" title="Wishlist" id="{{ $product->id }}" onclick="addToWishList(this.id)">
